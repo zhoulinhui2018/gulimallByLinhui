47d6cc24d62e754e7a7b025623fd435d58194853
zhoulinhui <1047043261@qq.com>
2020-07-29 15:28:13 +0800

    项目初始化

---
+++ gulimall-user/src/main/java/com/linhui/gulimall/user/GulimallUserApplication.java
@@ -0,0 +1,13 @@
+package com.linhui.gulimall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallUserApplication.class, args);
+    }
+
+}
---
+++ gulimall-user/src/test/java/com/linhui/gulimall/user/GulimallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhui.gulimall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
